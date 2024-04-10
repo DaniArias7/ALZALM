e8b8517d2670cc9bd65081105845f215fe5d0069
DaniArias7 <129128034+DaniArias7@users.noreply.github.com>
2024-04-09 22:12:42 -0500

    Create two2.py

---
+++ two2.py
@@ -0,0 +1,21 @@
+def decimal_a_hexadecimal(decimal):
+    hex_map = {
+        10: 'A',
+        11: 'B',
+        12: 'C',
+        13: 'D',
+        14: 'E',
+        15: 'F'
+    }
+
+    if decimal < 10:
+        return str(decimal)
+    elif decimal in hex_map:
+        return hex_map[decimal]
+    else:
+        cociente = decimal // 16
+        residuo = decimal % 16
+        return decimal_a_hexadecimal(cociente) + decimal_a_hexadecimal(residuo)
+
+numero_decimal = 710
+print("El número decimal", numero_decimal, "en hexadecimal es:", decimal_a_hexadecimal(numero_decimal))
