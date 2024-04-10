52b3f7ede9e2c07170aceca7e731f3e3f7b05dc4
DaniArias7 <129128034+DaniArias7@users.noreply.github.com>
2024-04-09 22:36:33 -0500

    Update apse.py

--- apse.py
+++ apse.py
@@ -1,9 +1,11 @@
+#O(n!)
+
 import itertools
 
-cifras_impares = [1, 3, 5, 7, 9]
+cifras_impares = [1, 3, 5, 7, 9] # 0(1)
 
-combinaciones = itertools.permutations(cifras_impares, 5)
+combinaciones = itertools.permutations(cifras_impares, 5) # O(n!)
 
-total_combinaciones = sum(1 for _ in combinaciones)
+total_combinaciones = sum(1 for _ in combinaciones) # O(n!)
 
-print("El número total de números de cinco cifras distintas con las cifras impares es:", total_combinaciones)
+print("El número total de números de cinco cifras distintas con las cifras impares es:", total_combinaciones) # 0(1)
